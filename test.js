9af9b36d8fe4abc67a4de81608b9bbeee7c49744
ay2306 <mahajan.ayush2306@gmail.com>
2020-09-20 01:01:30 +0530

    asd

--- test.js
+++ test.js
@@ -1,7 +1,14 @@
 const quote=require('./index.js');
 
 var Quote = quote.getQuote();
+if(!Quote.hasOwnProperty('text'))throw new Error("Quote does not have \'text\' key");
+if(!Quote.hasOwnProperty('author'))throw new Error("Quote does not have \'author\' key");
 
+try{
+    require('./data/data.json')
+}catch(e){
+    throw new Error("Incorrect JSON");
+}
 /* getQuote() returns an object containing 'text' and 'author' */
 
 console.log('\n***********************************');
